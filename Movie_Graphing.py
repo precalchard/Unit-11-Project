b4b99c41c7facd79a6e5b3d6e39e3af0552edefb
precalchard <seaphantom07@gmail.com>
2025-02-05 16:22:06 +0000

    Worked on basic graphing attempt

---
+++ Movie_Graphing.py
@@ -0,0 +1,10 @@
+#age_rating is ranked, so G=0, PG= 1, PG-13=2, etc.
+
+import pandas
+import matplotlib.pyplot as plot
+
+graph = pandas.read_csv("Movie_Data.csv")
+
+graph.plot(x="year", y="rank", m="rating")
+
+plot.savefig("Graph_attempt_1.png") 
