b24d9c1f11b366a1ac6f634b53067958123276d5
precalchard <seaphantom07@gmail.com>
2025-02-06 16:22:22 +0000

    Updated data

--- Movie_Graphing.py
+++ Movie_Graphing.py
@@ -1,4 +1,4 @@
-#age_rating is ranked, so G=0, PG= 1, PG-13=2, etc.
+#age_rating is ranked, so Passed = -2,Not Rated = -1, G=0, PG= 1, PG-13=2, etc. 
 
 import pandas
 import matplotlib.pyplot as plot
