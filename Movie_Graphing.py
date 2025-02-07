8c50d83df51241e842129bd593c53003134d26e1
precalchard <seaphantom07@gmail.com>
2025-02-07 00:54:38 +0000

    Attemped to work on means

--- Movie_Graphing.py
+++ Movie_Graphing.py
@@ -1,7 +1,11 @@
 #age_rating is ranked, so Passed = -2,Not Rated = -1, G=0, PG= 1, PG-13=2, etc. 
 
 import pandas
+import numpy as np
 import matplotlib.pyplot as plot
+
+avg_runtime = np.mean("run_time")
+print(avg_runtime)
 
 graph = pandas.read_csv("Movie_Data.csv")
 
