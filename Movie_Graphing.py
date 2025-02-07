b8e488592841c52f37eb04593735b8b72e0591f2
precalchard <seaphantom07@gmail.com>
2025-02-07 16:23:12 +0000

    Avreage run_time worked

--- Movie_Graphing.py
+++ Movie_Graphing.py
@@ -1,14 +1,13 @@
-#age_rating is ranked, so Passed = -2,Not Rated = -1, G=0, PG= 1, PG-13=2, etc. 
-
 import pandas
-import numpy as np
 import matplotlib.pyplot as plot
-
-avg_runtime = np.mean("run_time")
-print(avg_runtime)
 
 graph = pandas.read_csv("Movie_Data.csv")
 
-graph.plot(x="year", y="rank", m="rating")
+def avg(col):
+    total = sum(col)/len(col)
+    return total
+print(f"The avreage run time for the top 250 movies is {avg(graph["run_time"])} minutes.")
+
+#graph.plot(x="year", y="rank", m="rating")
 
 plot.savefig("Graph_attempt_1.png") 
